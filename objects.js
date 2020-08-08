a63766e5ece17b40161584b5d78f91d764b4c3b4
Cale Neff <cale.neff@yahoo.com>
2020-08-08 22:40:31 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = {blackEyePeas: "Boom Boom Pow"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, {[artistName]: songTitle});
+  playlist[artistName] = songTitle;
+  return playlist;
 }
